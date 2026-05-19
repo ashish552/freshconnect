2263711826ef99e078864a6d159a7befbb93268c
ashish552 <78848681+ashish552@users.noreply.github.com>
2026-05-19 19:19:24 +1200

    Implemented user registration API

--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -1,12 +1,12 @@
 from extensions import db
 
 class User(db.Model):
-    __tabelname__ = 'users'
+    __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100), nullable = False)
     email = db.Column(db.String(100), unique = True, nullable = False)
-    password = db.Column(db.String(120), nullable = False)
+    password = db.Column(db.Text, nullable = False)
     role = db.Column(db.String(40), nullable = False)
     
     def __repr__(self):
