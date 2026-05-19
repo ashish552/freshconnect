fa0e24a04da782e38336fd423e4a92e5df756fbe
ashish552 <78848681+ashish552@users.noreply.github.com>
2026-05-19 19:36:27 +1200

    Implemented login API with JWT authentication

--- backend/routes/auth_routes.py
+++ backend/routes/auth_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token
 
 from extensions import db
 from models.user_model import User
@@ -36,3 +37,38 @@ def register():
     return jsonify({
         "message": "User registerd successfully"
     }), 201
+@auth_bp.route('/login', methods = ['POST'])
+def login():
+    data = request.get_json()
+
+    email = data.get('email')
+    password = data.get('password')
+
+    user = User.query.filter_by(email = email).first()
+
+    if not user:
+        return jsonify({
+            "message":"Invalid email or password"
+        }), 401
+    if not check_password_hash(user.password, password):
+        return jsonify({
+            "message":"Not valid id or password"
+        }), 401
+    
+    access_token = create_access_token(identity={
+        'id': user.id,
+        'email': user.email,
+        'role': user.role
+    })
+
+    return jsonify({
+        "message": "Login successful",
+        "token": access_token,
+        "user": {
+            "id": user.id,
+            "name": user.name,
+            "email": user.email,
+            "role": user.role 
+        }
+    }),200
+    
